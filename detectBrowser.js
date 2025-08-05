165c668f310986ecf23d97eb87c5db786bcd6070
Artery-cmd <140864642+Artery-cmd@users.noreply.github.com>
2025-08-05 16:09:38 +0100

    Update detectBrowser.js

--- detectBrowser.js
+++ detectBrowser.js
@@ -7,13 +7,9 @@ function detectBrowser() {
     { name: 'Vivaldi', check: () => ua.includes("Vivaldi") },
     { name: 'Waterfox', check: () => ua.includes("Waterfox") },
     { name: 'Brave', check: () => ua.includes("Brave") || navigator.brave && typeof navigator.brave.isBrave === 'function' && navigator.brave.isBrave() },
-    { name: 'Ungoogled', check: () => !navigator.doNotTrack && !navigator.brave && ua.includes("Chrome") }, // DonotTrack is removed + Uses Chromium
-    { name: 'Librefox', check: () => !navigator.doNotTrack && !navigator.brave && ua.includes("Firefox") }, // DonotTrack is removed + Uses Firefox
     { name: 'Firefox', check: () => ua.includes("Firefox/") }, 
     { name: 'Safari', check: () => ua.includes("Safari") && !ua.includes("Chrome") && !ua.includes("Chromium") },
     { name: 'Chrome', check: () => ua.includes("Chrome") && ua.includes("Safari") && !ua.includes("Edg") && !ua.includes("OPR") && !ua.includes("Vivaldi") },
-    { name: 'Selenium', check: () => navigator.webdriver }, // Selenium webdriver protocol
-    { name: 'Puppeteer', check: () => navigator.webdriver === undefined && navigator.plugins.length === 0 && navigator.languages.length === 0  && window.chrome && !window.chrome.runtime}, // Puppeteer has no plugins, languages are empty
     { name: 'Arc', check: () => ua.includes("Arc") },
     { name: 'DuckDuckGo', check: () => ua.includes("DuckDuckGo") },
     { name: 'Samsung Internet', check: () => ua.includes("SamsungBrowser") },
@@ -25,7 +21,13 @@ function detectBrowser() {
     { name: 'Basilisk', check: () => ua.includes("Basilisk")},
     { name: 'UCBrowser', check: () => ua.includes("UCBrowser")},
     { name: 'Iridium Browser', check: () => ua.includes("Iridium")},
+    { name: 'Selenium', check: () => navigator.webdriver }, // Selenium webdriver protocol
+    { name: 'Puppeteer', check: () => navigator.webdriver === undefined && navigator.plugins.length === 0 && navigator.languages.length === 0  && window.chrome && !window.chrome.runtime}, // Puppeteer has no plugins, languages are empty
     //Add extra browsers here:
+    
+    //Less Reliable Detection:
+    { name: 'Librefox', check: () => !navigator.doNotTrack && !navigator.brave && ua.includes("Firefox") && typeof window.sidebar === 'undefined'}, // DonotTrack is removed + Uses Firefox
+    { name: 'Ungoogled', check: () => !navigator.doNotTrack && ua.includes("Chrome") && !ua.includes("Google") && !window.chrome?.runtime && !navigator.brave && !ua.includes("Vivaldi") && !ua.includes("Edge") && !ua.includes("Opera") }, // DonotTrack is removed + Uses Chromium
     { name: 'Unknown', check: () => true }
   ];
 
